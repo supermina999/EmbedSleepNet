863ca7813d330c7ff86a5de429662dd09a221009
Stanislav Minakov <supermina999@gmail.com>
2021-04-11 15:49:46 -0700

    Training works

--- model.py
+++ model.py
@@ -1,5 +1,10 @@
 import torch.nn.functional as F
+import torch.optim
 from torch import nn
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from dataset import load_split_sleep_dataset
 
 
 class TinySleepNetCNN(nn.Module):
@@ -20,6 +25,8 @@ class TinySleepNetCNN(nn.Module):
         self.dropout2 = nn.Dropout()
 
     def forward(self, x):
+        old_shape = x.shape
+        x = x.reshape((x.shape[0] * x.shape[1], 1, -1))
         x = F.relu(self.batchnorm1(self.conv1(x)))
         x = self.dropout1(self.maxpool1(x))
         x = F.relu(self.batchnorm2(self.conv2(x)))
@@ -28,6 +35,7 @@ class TinySleepNetCNN(nn.Module):
         x = self.maxpool2(x)
         x = self.flatten(x)
         x = self.dropout2(x)
+        x = x.reshape(old_shape[0], old_shape[1], -1)
 
         return x
 
@@ -35,12 +43,56 @@ class TinySleepNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.cnn = TinySleepNetCNN()
-        self.lstm = nn.LSTM(input_size=120, hidden_size=128)
+        self.lstm = nn.LSTM(input_size=120, hidden_size=128, batch_first=True)
         self.fc = nn.Linear(128, 5)
 
     def forward(self, x):
-        out = self.cnn(x).unsqueeze(1)
-        out, _ = self.lstm(out)
-        out = self.fc(out).squeeze(1)
+        x = self.cnn(x)
+        x, _ = self.lstm(x)
+        x = self.fc(x).squeeze(1)
         return x
 
+
+class LightningModule(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.net = TinySleepNet()
+        self.criterion = nn.CrossEntropyLoss()
+        self.learning_rate = 1e-3
+
+    def forward(self, x):
+        return self.net(x)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def training_step(self, train_batch, batch_idx):
+        data, labels = train_batch
+        outputs = self(data)
+        outputs = outputs.reshape(-1, outputs.shape[2])
+        labels = labels.reshape(-1)
+        loss = self.criterion(outputs, labels)
+        self.log('train_loss', loss)
+        return {'loss': loss}
+
+    def validation_step(self, val_batch, batch_idx):
+        data, labels = val_batch
+        outputs = self(data)
+        outputs = outputs.reshape(-1, outputs.shape[len(outputs.shape) - 1])
+        labels = labels.reshape(-1)
+        loss = self.criterion(outputs, labels)
+        self.log('val_loss', loss.item(), prog_bar=True)
+        return {'val_loss': loss.item()}
+
+    def prepare_data(self):
+        self.train_ds, self.val_ds = load_split_sleep_dataset()
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, shuffle=True, batch_size=3)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=64)
+
+    def on_train_epoch_start(self):
+        self.train_ds.reshuffle()
+        self.val_ds.reshuffle()
--- train.py
+++ train.py
@@ -1,33 +1,11 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import random_split, DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
 
-from model import TinySleepNet
-from dataset import SleepDataset
+from model import LightningModule
 
-dataset = SleepDataset()
-train_size = int(len(dataset) * 0.8)
-train_ds, test_ds = random_split(dataset, (train_size, len(dataset) - train_size), generator=torch.Generator().manual_seed(42))
-train_loader = DataLoader(train_ds, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_ds, batch_size=64)
+model = LightningModule()
 
-model = TinySleepNet()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-for epoch in range(100):
-    running_loss = 0.0
-    for i, data in enumerate(train_loader):
-        inputs, labels = data
-        optimizer.zero_grad()
-
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+tb_logger = pl_loggers.TensorBoardLogger('logs/')
+trainer = pl.Trainer(logger=tb_logger, reload_dataloaders_every_epoch=True, gpus=1, auto_lr_find=True)
+trainer.tune(model)
+trainer.fit(model)
