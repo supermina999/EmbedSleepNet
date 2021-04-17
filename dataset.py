37109587a266ba21d65293529b88b07abf8e9c51
Stanislav Minakov <supermina999@gmail.com>
2021-04-17 16:38:33 -0700

    Improve batching

--- dataset.py
+++ dataset.py
@@ -5,35 +5,37 @@ from torch.utils.data import Dataset
 import random
 
 
-class SleepDataset(Dataset):
-    def __init__(self, data, labels, minibatch_size=20):
-        total_len = 0
-        for subj_data in data:
-            total_len += subj_data.shape[0] // minibatch_size
+def load_sleep_data(data_path='preprocessed'):
+    data = []
+    labels = []
+    for file in os.listdir(data_path):
+        if not file.endswith('.npz'):
+            continue
+        path = os.path.join(data_path, file)
+        loaded = np.load(path)
+        data.append(loaded['x'])
+        labels.append(loaded['y'])
+    return data, labels
 
+
+class BatchedSleepDataset(Dataset):
+    def __init__(self, data, labels, minibatch_size=20):
         self.data = data
         self.labels = labels
         self.minibatch_size = minibatch_size
-        self.total_len = total_len
         self.reshuffle()
-
-    @classmethod
-    def load_data(cls, data_path='preprocessed'):
-        data = []
-        labels = []
-        for file in os.listdir(data_path):
-            if not file.endswith('.npz'):
-                continue
-            path = os.path.join(data_path, file)
-            loaded = np.load(path)
-            data.append(loaded['x'])
-            labels.append(loaded['y'])
-        return data, labels
 
     def reshuffle(self):
         self.shifts = []
+        self.cur_lens = []
+        self.total_len = 0
         for subj_data in self.data:
-            self.shifts.append(random.randint(0, subj_data.shape[0] % self.minibatch_size))
+            max_skip = 5 * self.minibatch_size + subj_data.shape[0] % self.minibatch_size
+            cur_skip = random.randint(0, max_skip)
+            self.shifts.append(cur_skip)
+            cur_len = (subj_data.shape[0] - cur_skip) // self.minibatch_size
+            self.cur_lens.append(cur_len)
+            self.total_len += cur_len
 
     def __len__(self):
         return self.total_len
@@ -41,21 +43,35 @@ class SleepDataset(Dataset):
     def __getitem__(self, index):
         subj_idx = len(self.data) - 1
         for idx, subj_data in enumerate(self.data):
-            cur_len = subj_data.shape[0] // self.minibatch_size
+            cur_len = self.cur_lens[idx]
             if index >= cur_len:
                 index -= cur_len
             else:
                 subj_idx = idx
                 break
         start_idx = self.shifts[subj_idx] + index * self.minibatch_size
-        item_data = self.data[subj_idx][start_idx:start_idx+self.minibatch_size]
-        item_labels = self.labels[subj_idx][start_idx:start_idx+self.minibatch_size]
-        return torch.tensor(item_data, dtype=torch.float).unsqueeze(1), torch.tensor(item_labels, dtype=torch.long).unsqueeze(1)
+        item_data = self.data[subj_idx][start_idx:start_idx + self.minibatch_size]
+        item_labels = self.labels[subj_idx][start_idx:start_idx + self.minibatch_size]
+        return torch.tensor(item_data, dtype=torch.float).unsqueeze(1), \
+               torch.tensor(item_labels, dtype=torch.long).unsqueeze(1)
 
 
-def load_split_sleep_dataset(data_path='preprocessed', minibatch_size=20, train_coef=0.8):
-    data, labels = SleepDataset.load_data(data_path)
-    train_size = round(len(data) * train_coef)
+class SleepDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return torch.tensor(self.data[index], dtype=torch.float).unsqueeze(1), \
+               torch.tensor(self.labels[index], dtype=torch.long).unsqueeze(1)
+
+
+def load_split_sleep_dataset(data_path='preprocessed', minibatch_size=20):
+    data, labels = load_sleep_data(data_path)
+    train_size = round(len(data) - 8)
     data_train, labels_train = data[:train_size], labels[:train_size]
     data_test, labels_test = data[train_size:], labels[train_size:]
-    return SleepDataset(data_train, labels_train, minibatch_size), SleepDataset(data_test, labels_test, minibatch_size)
+    return BatchedSleepDataset(data_train, labels_train, minibatch_size), SleepDataset(data_test, labels_test)
