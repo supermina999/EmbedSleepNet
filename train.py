95c0a189d271ca5612e24742d5b79d7c63acd86a
Stanislav Minakov <supermina999@gmail.com>
2021-04-17 16:47:33 -0700

    Removed unused models, refactor

--- train.py
+++ train.py
@@ -2,12 +2,12 @@ import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 import random
 
-from model import TinySleepNet
+from model import TinySleepNet, EmbedSleepNet
 from lightning_wrapper import LightningWrapper
 
 random.seed(42)
 
-model = LightningWrapper(TinySleepNet())
+model = LightningWrapper(EmbedSleepNet())
 
 tb_logger = pl_loggers.TensorBoardLogger('logs/')
 trainer = pl.Trainer(logger=tb_logger, reload_dataloaders_every_epoch=True, gpus=1)
